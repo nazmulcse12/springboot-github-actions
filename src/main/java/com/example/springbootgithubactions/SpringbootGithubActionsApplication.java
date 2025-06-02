d6da69e6c029c1bfb7afda49c139f5b670e9c5fd
nazmulcse12 <nazmul.csejust@gmail.com>
2025-06-02 12:57:09 +0600

    Initial commit with GitHub Actions

---
+++ src/main/java/com/example/springbootgithubactions/SpringbootGithubActionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootgithubactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGithubActionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGithubActionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootgithubactions/SpringbootGithubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootgithubactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGithubActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
